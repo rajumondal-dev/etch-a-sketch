814b9dc80d18dd489af748749a6d808c3e1c4610
Raju Mondal <rajumondal.connect@gmail.com>
2025-12-18 18:16:21 +0530

    Add the button to set pixel amont

--- script.js
+++ script.js
@@ -17,3 +17,35 @@ allPixels.forEach(pixel=>{
 
 createPixels(16);
 
+const setPixels = document.createElement("button");
+setPixels.setAttribute("id","setPixels");
+setPixels.textContent = "Set Pixels";
+document.body.insertBefore(setPixels,container);
+
+setPixels.addEventListener("click", () =>{
+
+    const userInput = parseInt(prompt("Set how many squares you want per side","16"));
+
+    if(userInput>100){
+        alert("Maximum legth is 100");
+    };
+
+    const allPixels = document.querySelectorAll(".pixel");
+    allPixels.forEach(element => {
+        element.remove();
+    })
+    createPixels(userInput);
+});
+
+const clear = document.createElement("button");
+clear.setAttribute("id","clear");
+clear.textContent = "Clear";
+document.body.insertBefore(clear,container);
+
+
+clear.addEventListener("click", () => {
+    const allPixels = document.querySelectorAll(".pixel");
+    allPixels.forEach(element => {
+        element.style.backgroundColor = "white";
+    })
+})
